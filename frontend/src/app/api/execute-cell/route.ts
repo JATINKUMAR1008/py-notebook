398ae0e19f4917f94536937ec1df0e2b385d18f0
Jatin Kumar <jatinkumar10082003@gmail.com>
2024-10-15 15:43:51 +0530

    added new features

--- frontend/src/app/api/execute-cell/route.ts
+++ frontend/src/app/api/execute-cell/route.ts
@@ -3,13 +3,16 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   if (req.method === "POST") {
     try {
-      const response = await fetch("http://localhost:8000/execute-cell", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(await req.json()),
-      });
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API}/execute-cell`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(await req.json()),
+        }
+      );
       const result = await response.json();
 
       return NextResponse.json({ output: result.output });
